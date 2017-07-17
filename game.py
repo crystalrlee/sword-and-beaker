c5c3f62463549e971bed87a40cf92d665a120fc7
Crystal Lee <crystalrosemayer@gmail.com>
2017-07-16 18:32:22 -0700

    Made a window! Nothing else yet.

---
+++ game.py
@@ -0,0 +1,13 @@
+# Sword & Beaker adventure game
+
+import tkinter
+
+def main():
+    # creates a tk window object, names it window
+    window = tkinter.Tk()
+    window.title("Sword & Beaker")
+    window.geometry("1200x800")
+    # opens the window
+    window.mainloop()
+
+main()
