981c218d5e0c230bfae40e4aa2b95dfff47d08a0
Crystal Lee <crystalrosemayer@gmail.com>
2017-07-17 21:33:02 -0700

    Added game text field and input button, all still in testing phase.

--- game.py
+++ game.py
@@ -12,6 +12,17 @@ def get_avatar_image(window):
     label_avatar.pack(side = "left")
     return label_avatar
 
+def get_input_text_field(window):
+    input_text_field = tkinter.Entry(window)
+    input_text_field.pack()
+    return input_text_field
+
+def get_game_text(window):
+    game_text = tkinter.Label(window, text = "Testing... how much text can go here?",
+        fg = "green2", bg = "grey1", height = "40", width = "80" )
+    game_text.pack()
+    return game_text
+
 def main():
     # creates a tk window object
     window = tkinter.Tk()
@@ -21,10 +32,11 @@ def main():
     # loads avatar image
     avatar_image = get_avatar_image(window)
 
+    #loads game text and input text field
+    game_text = get_game_text(window)
+    input_text_field = get_input_text_field(window)
 
     # opens the window
     window.mainloop()
-
-
 
 main()
