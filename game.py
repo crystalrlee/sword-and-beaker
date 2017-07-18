042734c040a7370d9a3d205e80984bead67d3d40
Crystal Lee <crystalrosemayer@gmail.com>
2017-07-17 21:43:13 -0700

    Added submit button, changed import tkinter to from tkinter import *

--- game.py
+++ game.py
@@ -1,40 +1,46 @@
 # game.py
 # Sword & Beaker adventure game
 
-import tkinter
+from tkinter import *
 
 # Sets image for character
 def get_avatar_image(window):
-    avatar_image = tkinter.PhotoImage(file = "warrior_girl.gif")
-    label_avatar = tkinter.Label(window, image = avatar_image)
+    avatar_image = PhotoImage(file = "warrior_girl.gif")
+    label_avatar = Label(window, image = avatar_image)
     # Make sure photo piggybacks on label so it doesn't get garbage collected
     label_avatar.avatar_image = avatar_image
     label_avatar.pack(side = "left")
     return label_avatar
 
 def get_input_text_field(window):
-    input_text_field = tkinter.Entry(window)
+    input_text_field = Entry(window)
     input_text_field.pack()
     return input_text_field
 
 def get_game_text(window):
-    game_text = tkinter.Label(window, text = "Testing... how much text can go here?",
+    game_text = Label(window, text = "Testing... how much text can go here?",
         fg = "green2", bg = "grey1", height = "40", width = "80" )
     game_text.pack()
     return game_text
 
+def get_submit_button(window):
+    submit_button = Button(window, text="Submit", state = "disabled")
+    submit_button.pack()
+    return submit_button
+
 def main():
     # creates a tk window object
-    window = tkinter.Tk()
+    window = Tk()
     window.title("Sword & Beaker")
     window.geometry("1400x800")
 
     # loads avatar image
     avatar_image = get_avatar_image(window)
 
-    #loads game text and input text field
+    #loads game text and input text field and submit button
     game_text = get_game_text(window)
     input_text_field = get_input_text_field(window)
+    submit_button = get_submit_button(window)
 
     # opens the window
     window.mainloop()
