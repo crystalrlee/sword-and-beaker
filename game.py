117e45ddddc1e16000b0dffb6b3660b3e2a0886d
Crystal Lee <crystalrosemayer@gmail.com>
2017-07-17 20:38:33 -0700

    Added temporary avatar image, learned how to pack it on left side of window

--- game.py
+++ game.py
@@ -1,13 +1,30 @@
+# game.py
 # Sword & Beaker adventure game
 
 import tkinter
 
+# Sets image for character
+def get_avatar_image(window):
+    avatar_image = tkinter.PhotoImage(file = "warrior_girl.gif")
+    label_avatar = tkinter.Label(window, image = avatar_image)
+    # Make sure photo piggybacks on label so it doesn't get garbage collected
+    label_avatar.avatar_image = avatar_image
+    label_avatar.pack(side = "left")
+    return label_avatar
+
 def main():
-    # creates a tk window object, names it window
+    # creates a tk window object
     window = tkinter.Tk()
     window.title("Sword & Beaker")
-    window.geometry("1200x800")
+    window.geometry("1400x800")
+
+    # loads avatar image
+    avatar_image = get_avatar_image(window)
+
+
     # opens the window
     window.mainloop()
+
+
 
 main()
