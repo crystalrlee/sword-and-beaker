1532f9448e425987d91891037528013fb94092ff
Crystal Lee <crystalrosemayer@gmail.com>
2017-07-24 22:25:38 -0700

    Big changes! Redid all graphics layout, to include map in main root window. Started on the second level, the mountains! Added graphics for map.

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ class Game():
         self.root = root
         # Sets root window properties
         root.title("Sword & Beaker")
-        root.geometry("1100x530")
+        root.geometry("1150x635")
         root.resizable(False, False) # Makes window fixed size
 
         # Main container frame properties
@@ -17,19 +17,27 @@ class Game():
         main_container.grid()
 
         # Avatar label
-        self.avatar_image = PhotoImage(file = "warrior_girl.gif")
-        self.avatar = Label(main_container, image = self.avatar_image, bd = 10)
-        self.avatar.grid(column = 0, row = 0)
+        self.avatar_image = PhotoImage(file = "avatar.gif")
+        self.avatar = Label(main_container, image = self.avatar_image, borderwidth = 0)
+        self.avatar.grid(column = 5, row = 2, rowspan = 4)
+
+        # Sets up map, loads images
+        self.map_image = PhotoImage(file = "map-city-only.gif")
+        self.map_image2 = PhotoImage(file = "map-city-mountains.gif")
+        self.map_image3 = PhotoImage(file = "map-city-mountains-antarctica.gif")
+        self.map_image4 = PhotoImage(file = "map-all-color1.gif")
+        self.map_label = Label(main_container, image = self.map_image, borderwidth = 5)
+        self.map_label.grid(column = 4, columnspan = 2, row = 0)
+        self.map_label.bind("<Button-1>", self.on_map_click)
 
         # Game text area
-        self.game_text = Label(main_container, fg = "green2", bg = "grey1", wraplength = "500", height = "28", width = "92",
-            font = ("Liberation Mono", 10), text = (
+        self.game_text = Label(main_container, fg = "green2", bg = "grey1", width = 68, height = 29, wraplength = 475,
+            font = ("Liberation Mono", 12), text = (
             "Sword & Beaker\nA Python Adventure Game\n\n"
             "You are a scientist whose funding has been cut."
-            "You must travel from the City to the Mountains to Antarctica, battling enemies along the way "
-            "in order to find your missing lab equipment.\n"
+            "In order to find your missing lab equipment you must travel from the City to the Mountains to Antarctica, battling enemies along the way.\n\n"
             "Click on the Map to get started.\n\nGood luck!"))
-        self.game_text.grid(column = 1, row = 0, columnspan = 5)
+        self.game_text.grid(column = 0, row = 0, rowspan = 3, columnspan = 4)
 
         # State variables: coffee count, health, max health, location, lab equipment count
         self.coffee = 1
@@ -40,61 +48,54 @@ class Game():
         self.equipment_count = 0
 
         # Buttons
-        self.map_button = Button(main_container, text="MAP", command = self.open_map)
-        self.map_button.grid(column = 1, row = 2)
         self.coffee_button = Button(main_container, text="DRINK COFFEE", state = DISABLED, command = self.drink_coffee)
-        self.coffee_button.grid(column = 2, row = 2)
+        self.coffee_button.grid(column = 0, row = 4)
         self.seek_fight_button = Button(main_container, text="PICK A FIGHT", state = DISABLED, command = self.fight_setup)
-        self.seek_fight_button.grid(column = 3, row = 2)
+        self.seek_fight_button.grid(column = 1, row = 4)
         self.attack_button = Button(main_container, text = "ATTACK!", state = DISABLED)
-        self.attack_button.grid(column = 4, row = 2)
+        self.attack_button.grid(column = 2, row = 4)
         self.run_away_button = Button(main_container, text="RUN AWAY!", state = DISABLED)
-        self.run_away_button.grid(column = 5, row = 2)
+        self.run_away_button.grid(column = 3, row = 4)
 
         # Health/Strength/Equipment Stats//Coffee count//Your location
-        self.health_stats = Label(main_container, text = "Health: {}/{}".format(self.health, self.max_health),  bg = "white", font = ("Helvetica", 14))
-        self.health_stats.grid(column = 0, row = 1)
-        self.strength_stats = Label(main_container, text = "Strength: {}".format(self.strength), font = ("Helvetica", 14), bg = "white")
-        self.strength_stats.grid(column = 0, row = 2)
-        self.equipment_stats = Label(main_container, text = "Lab Equipment: {}/3".format(self.equipment_count), font = ("Helvetica", 14), bg = "white")
-        self.equipment_stats.grid(column = 0, row = 3)
-        self.coffee_count = Label(main_container, text = "Coffee: {}".format(self.coffee), bg = "white")
-        self.coffee_count.grid(column = 2, row = 3)
-        self.location_stats = Label(main_container, text = "Location: {}".format(self.location), bg = "white")
-        self.location_stats.grid(column = 1, row = 3)
+        self.stats_frame = Frame(main_container, bg = "papaya whip")    # Sets frame to hold Stats
+        self.stats_frame.grid(column = 4, row = 1, rowspan = 5)
+        self.location_stats = Label(self.stats_frame, text = "Location: {}".format(self.location), bg = "papaya whip", font = ("Helvetica", 14))
+        self.location_stats.grid(column = 4, row = 1)
+        self.health_stats = Label(self.stats_frame, text = "Health: {}/{}".format(self.health, self.max_health),  bg = "papaya whip", font = ("Helvetica", 14))
+        self.health_stats.grid(column = 4, row = 2)
+        self.strength_stats = Label(self.stats_frame, text = "Strength: {}".format(self.strength), font = ("Helvetica", 14), bg = "papaya whip")
+        self.strength_stats.grid(column = 4, row = 3)
+        self.equipment_stats = Label(self.stats_frame, text = "Lab Equipment: {}/3".format(self.equipment_count), font = ("Helvetica", 14), bg = "papaya whip")
+        self.equipment_stats.grid(column = 4, row = 4)
+        self.coffee_count = Label(self.stats_frame, text = "Coffee: {}".format(self.coffee), bg = "papaya whip", font = ("Helvetica", 14))
+        self.coffee_count.grid(column = 4, row = 5)
 
-    # Called during a fight to update health stats
-    def fight_health_stats(self):
-        self.health_stats.config(text = "Health: {}/{}".format(self.health, self.max_health))
+    # Figures out where user clicked, sends to correct land and/or gives unlock message
+    def on_map_click(self, event):
+        # user clicked The City
+        if self.equipment_count == 0:
+            if 28 <= event.x <= 128 and 88 <= event.y <= 200:
+                self.enter_the_city(event)
+            else:
+                self.game_text.config(text = "That area isn't unlocked yet. Try picking a fight to find more lab equipment.")
+        elif 287 <= event.x <= 482 and 74 <= event.y <= 170:
+            self.enter_the_mountains()
 
-    def disable_buttons(self):
-        self.map_button.config(state = DISABLED)
-        self.coffee_button.config(state = DISABLED)
-        self.seek_fight_button.config(state = DISABLED)
-        self.run_away_button.config(state = DISABLED)
+    # user clicks on the mountains when it's unlocked
+    def enter_the_mountains(self):
+        # Changes game text
+        self.game_text.config(text = (
+            "You enter \"The Mountains\"!\n\n"
+            "From here you can heal your wounds with a cup of coffee,\n"
+            "pick a fight with the locals, \n"
+            "or go somewhere else on your map. \n\n"
+            ))
+        self.location = "The Mountains"
+        self.location_stats.config(text = "Location: {}".format(self.location))
 
-    def enable_buttons(self):
-        self.map_button.config(state = NORMAL)
-        self.coffee_button.config(state = NORMAL)
-        self.seek_fight_button.config(state = NORMAL)
-        self.run_away_button.config(state = DISABLED)
-        self.attack_button.config(state = DISABLED)
-
-    # Sets up map window, displays map image, disables game buttons
-    def open_map(self):
-        self.map_window = Toplevel(width = 600, height = 400)
-        self.map_window.resizable(False, False)
-        self.map_window.title("Map")
-        self.map_image = PhotoImage(file = "map-all.gif")
-        self.background_label = Label(self.map_window, image = self.map_image)
-        self.background_label.bind("<Button-1>", self.enter_the_city)
-        self.background_label.bind("<Destroy>", self.enter_the_city)
-        self.background_label.pack()
-        self.disable_buttons()
-
+    # User clicks in The City
     def enter_the_city(self, event):
-        # Closes map window
-        self.map_window.destroy()
         # Enables buttons on root window
         self.enable_buttons()
         # Changes game text
@@ -102,10 +103,29 @@ class Game():
             "You enter \"The City\"!\n\nA concrete realm of sunshine, adventure, and gentrification.\n\n"
             "From here you can heal your wounds with a cup of coffee,\n"
             "pick a fight with the locals, \n"
-            "or go somewhere else on your map. \n\n"
-            "Enjoy it before your rent goes up again."))
+            "or go somewhere else on your map."
+            ))
         self.location = "The City"
         self.location_stats.config(text = "Location: {}".format(self.location))
+
+    # # Figures out where player clicks
+    # def get_click(self, event):
+    #     print("x-coordinate: {}, y-coordinate: {}".format(event.x, event.y))
+
+    # Called during a fight to update health stats
+    def fight_health_stats(self):
+        self.health_stats.config(text = "Health: {}/{}".format(self.health, self.max_health))
+
+    def disable_buttons(self):
+        self.coffee_button.config(state = DISABLED)
+        self.seek_fight_button.config(state = DISABLED)
+        self.run_away_button.config(state = DISABLED)
+
+    def enable_buttons(self):
+        self.coffee_button.config(state = NORMAL)
+        self.seek_fight_button.config(state = NORMAL)
+        self.run_away_button.config(state = DISABLED)
+        self.attack_button.config(state = DISABLED)
 
     # Checks location, populates enemies for that location
     def get_enemy(self):
@@ -115,14 +135,14 @@ class Game():
                 self.enemy = {
                     "name": "an Evil Landlord", "attack phrase": "They pummel you with eviction notices.",
                     "strength": 2, "health": 5}
-            if pick == 2:
+            if pick == 1:
                 self.enemy = {
                     "name": "a Pizza Rat", "attack phrase": "They attack you with clever memes.",
                     "strength": 3, "health": 4}
             else:
                 self.enemy = {
                     "name": "bedbugs", "attack phrase": "Did you know that bedbugs can survive for months without food or water?",
-                    "strength": 2, "health": 6}
+                    "strength": 1, "health": 6}
         else:
             pass
         return self.enemy
@@ -135,7 +155,7 @@ class Game():
         self.attack_button.config(state = NORMAL)
         self.enemy = self.get_enemy()
         # Encounter an ememy
-        self.fight_text = "You encounter {}.\n They have {} strength and {} health.\n\n".format(self.enemy["name"], self.enemy["strength"], self.enemy["health"])
+        self.fight_text = "You encounter {}.\n\n They have {} strength and {} health.\n\n".format(self.enemy["name"], self.enemy["strength"], self.enemy["health"])
         self.game_text.config(text = self.fight_text)
         self.attack_button.bind("<Button-1>", self.attack)
 
@@ -144,10 +164,10 @@ class Game():
         # Decides whether you attack first or the enemy gets the jump on you
         attack_first = random.randint(0,1)
         if attack_first == 0:
-            who_attacks_first = "You attack them with your lightning fast reflexes. They lose {} health.".format(self.strength)
+            who_attacks_first = "You attack them with your lightning fast reflexes.\n\nThey lose {} health.".format(self.strength)
             self.enemy["health"] -= self.strength
         else:
-            who_attacks_first = "{} You lose {} health.".format(self.enemy["attack phrase"], self.enemy["strength"])
+            who_attacks_first = "{}\n\nYou lose {} health.".format(self.enemy["attack phrase"], self.enemy["strength"])
             # Checks to see how much health you have (you can't go less than 0)
             if self.health > self.enemy["strength"]:
                 self.health -= self.enemy["strength"]
@@ -159,7 +179,7 @@ class Game():
         # Checks to see if player won fight, runs get lot funciton and outputs loot won
         if self.enemy["health"] <= 0:
             loot = self.get_loot()
-            self.game_text.config(text = "{}\n\nYou win! You get {}!".format(who_attacks_first, loot))
+            self.game_text.config(text = "{}\n\nYou win! You get {}".format(who_attacks_first, loot))
             self.enable_buttons()
         # Checks to see if player lost game
         elif self.health < 1:
@@ -173,43 +193,61 @@ class Game():
     def get_loot(self):
         loot = []
         # Checks if you win a piece of lab equipment; probablility 1:6
-        if self.location == "city" and self.equipment_count < 1:
-            special_item = "Electrostatic Analyzer"
+        if self.location == "The City" and self.equipment_count < 1:
+            special_item = "an Electrostatic Analyzer!\n The Mountains are unlocked."
             if random.randint(0,5) == 0:
-                self.equipment_count = 1
+                self.equipment_count += 1
                 self.equipment_stats.config(text = "Lab Equipment: {}/3".format(self.equipment_count))
-                loot.append("Electrostatic Analyzer")
+                loot.append(special_item)
+                self.map_label.config(image = self.map_image2)
         else:
             pass
+
         # Checks if you win coffee; probablility 1:3
-        if random.randint(0,1) == 0:
-            loot.append("a cup of coffee")
+        if random.randint(0,2) == 0:
+            loot.append("a cup of coffee.")
             self.coffee += 1
             self.coffee_count.config(text = "Coffee: {}".format(self.coffee))
-        # If player won nothing, let player know
-        if loot == []:
-            loot = "nothing."
+
+        # Data structures to hold possible max health and strength loot
+        possible_loot = {
+            "health item": [
+                "safety goggles. You feel much safer.\nYour max health increases.",
+                "a pocket protector. You're not sure what this does, but you feel much safer anyway.\n\nYour max health increases.",
+                "a kevlar lab coat. Once you figure out how to walk in this thing you're going to be unstoppable.\n\nYour max health increases.",
+                "rubber gloves.\n\nYour max health increases as your dexterity decreases."
+            ],
+            "strength item": [
+                "a basic sword. It's not a very good sword, but it is heavy.\n\nAfter carrying it for awhile you feel much stronger!",
+                "a robotic dog, an engineer's best friend.\n\nYour strength increases with its presence.",
+                "a robotic cat. It won't do what you tell it to do, but it has vicious claws.\n\nYour strength increases.",
+                "a gold-plated pipette. Good for poking others in the eye and not much else.\n\nYour strength increases."
+            ]
+        }
+
+        # Picks a random health item, checks if you win it, updates max health stats; probability 1:4
+        if random.randint(0,3) == 0:
+            loot.append(random.choice(possible_loot["health item"]))
+            self.max_health += 2
+            self.health_stats.config(text = "Health: {}/{}".format(self.health, self.max_health))
+
+        # Picks a random health item, checks if you win it, updates max health stats; probability 1:4
+        if random.randint(0,3) == 0:
+            loot.append(random.choice(possible_loot["strength item"]))
+            self.strength += 2
+            self.strength_stats.config(text = "Strength: {}".format(self.strength))
+
         # Gets a long string with all loot aquired
         loot_string = ""
-        for item in loot:
-            loot_string += item
+        for i in range(len(loot)):
+            if i == 0:
+                loot_string += "{}\n".format(loot[i])
+            else:
+                loot_string += "\nYou also won {}".format(loot[i])
+        # Makes sure player doesn't get an empty string
+        if loot_string == "":
+            loot_string = "nothing."
         return loot_string
-
-        # possible_loot = {
-        #     "health item": {
-        #         "safety goggles" : "You feel much safer. Your max health increases.",
-        #         "a pocket protector": "You're not sure what this does, but you feel much safer anyway.",
-        #         "a kevlar lab coat": "Once you figure out how to walk in this thing you're going to be unstoppable. Your max health increases."
-        #         "rubber gloves": "Your max health increases as your dexterity decreases."
-        #     }
-        #     "strength item": {
-        #         "a basic sword": "It's not a very good sword, but it is heavy. After carrying it for awhile you feel much stronger!",
-        #         "a robotic dog": "An engineer's best friend. Your strength increases with its presence.",
-        #         "a robotic cat": "It won't do what you tell it to do, but it has vicious claws. Your strength increases."
-        #         "a gold-plated pipette": "Good for poking others in the eye and not much else. Your strength increases."
-        #     }
-        # }
-        # REMEMBER TO PROMPT PLAYER TO TRY NEXT LEVEL IF THEY FOUND LAB EQUIPMENT!
 
     # Called when drink coffee button is clicked
     def drink_coffee(self):
